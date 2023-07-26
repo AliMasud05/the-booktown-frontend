50fcff7623ca523535e818d04bf516612588d269
AliMasud05 <morshedalimasud3@gmail.com>
2023-07-26 19:13:08 +0600

    route create

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import routes from './routes/routes'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={}>
-      <RouterProvider router={} />
+      <RouterProvider router={routes} />
     </Provider>
   </React.StrictMode>
 );
